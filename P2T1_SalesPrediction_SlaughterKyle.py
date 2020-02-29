74241f239540ccfe0a9af368faf5261a4de77262
KSlaughter0929 <61620166+KSlaughter0929@users.noreply.github.com>
2020-02-29 06:39:55 -0500

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_SlaughterKyle.py
@@ -0,0 +1,12 @@
+# Getting the projected amount of total sales.
+# February 24, 2020
+# CTI-110 P2T1 - Sales Prediction
+# Kyle Slaughter
+#
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
