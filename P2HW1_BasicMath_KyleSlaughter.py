a54f12147ecc015a0a54f174ae7ba55a0c74f89e
KSlaughter0929 <61620166+KSlaughter0929@users.noreply.github.com>
2020-03-09 07:19:01 -0400

    Add files via upload
    Fourth Submission

---
+++ P2HW1_BasicMath_KyleSlaughter.py
@@ -0,0 +1,21 @@
+# Adding, Multiplying, and finding the final result of both numbers.
+# February 24, 2020
+# CTI-110 P2HW1 - Basic Math
+# Kyle Slaughter
+#
+
+# Enter a number to the salary and bonus.
+first_number = float(input('Enter the first number '))
+second_number = float(input('Enter the second number '))
+
+# Adding the first and second number.
+total = first_number + second_number
+
+# Display the added total.
+print('Your added total is', total)
+
+# Multiplying first and second number.
+total = first_number * second_number
+
+# Display the multiplied total.
+print('Your multiplied total is', total)
